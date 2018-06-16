61d612ccf14337b98ef3ffc68eb741f8a3e12a86
Rob Burns <robburns3@gmail.com>
2018-06-16 06:31:51 -0400

    Completed albums tutorial
    REST endpoint, styling, open external browser, click events, scrolling.

--- albums/index.js
+++ albums/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 // Create a component
